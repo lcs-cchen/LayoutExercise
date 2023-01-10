c8b0988d27565528d724ae379aff0c231a5f6c73
cyrus chen <pschen@lcs.on.ca>
2023-01-10 09:20:12 -0500

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Cyrus Chen on 10/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
